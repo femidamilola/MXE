b63ba4d12d70557c83b04476347b185c0210c732
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-06 22:41:14 +0100

    setup auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
-import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma.service';
+import { MessageService } from 'src/utils/message.service';
 
 @Module({
-    controllers: [AuthController],
-    providers: [AuthService]
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService, MessageService],
 })
 export class AuthModule {}
