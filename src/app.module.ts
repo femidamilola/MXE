77efef66d156db29da2695b67ad5ad0e5e9f3df5
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-06 22:39:41 +0100

    setup config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { PrismaService } from './prisma.service';
+import { MessageService } from './utils/message.service';
+import { ConfigModule } from '@nestjs/config';
+import { UtilService } from './utils/util.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService, MessageService, UtilService],
 })
 export class AppModule {}
