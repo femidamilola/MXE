3c17c90a0f0a8d4c9d90ac842371fe82a09fd14b
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-06 22:41:47 +0100

    setup sms service

---
+++ src/utils/message.service.ts
@@ -0,0 +1,39 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+// import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+// import { Logger } from 'winston';
+import * as twilio from 'twilio';
+
+@Injectable()
+export class MessageService {
+  private readonly client: twilio.Twilio;
+  constructor(
+    private config: ConfigService,
+    // @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {
+    this.client = twilio(
+      config.get('TWILIO_ACCOUNT_SID'),
+      config.get('TWILIO_AUTH_TOKEN'),
+    );
+  }
+
+  /**
+   * send text message via twilio api
+   * @param to : reciever address
+   * @param body : body of message
+   */
+  async sendTestMessage(to: string, body: string) {
+    await this.client.messages
+      .create({
+        to: to,
+        body: body,
+        from: this.config.get('TWILIO_NUMBER'),
+      })
+      .catch((error) => {
+        throw new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      });
+  }
+}
