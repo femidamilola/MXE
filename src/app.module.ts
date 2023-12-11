fa759d8c9e0e1b01e9de8f843b49c7b357537a5c
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-11 22:45:15 +0100

    imported auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './prisma.service';
 import { MessageService } from './utils/message.service';
 import { ConfigModule } from '@nestjs/config';
 import { UtilService } from './utils/util.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   controllers: [AuthController],
   providers: [AuthService, PrismaService, MessageService, UtilService],
 })
