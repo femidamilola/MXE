a01052b8f4b6bb815d2fbc57879ea10d0ea42a5f
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-11 22:45:27 +0100

    setup auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,28 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma.service';
 import { MessageService } from 'src/utils/message.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UtilService } from 'src/utils/util.service';
+import { ConfigService } from '@nestjs/config';
+import { jwtConstants } from './constants';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '15m' },
+    }),
+    PassportModule.register({ defaultStrategy: 'google', session: false }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, MessageService],
+  providers: [
+    AuthService,
+    PrismaService,
+    MessageService,
+    UtilService,
+    ConfigService,
+  ],
 })
 export class AuthModule {}
