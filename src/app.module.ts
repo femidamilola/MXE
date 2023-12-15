beebc42da4ab1330e6365e66c30b540694401dbf
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-15 10:24:20 +0100

    imported wallet module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { MessageService } from './utils/message.service';
 import { ConfigModule } from '@nestjs/config';
 import { UtilService } from './utils/util.service';
 import { AuthModule } from './auth/auth.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, WalletModule],
   controllers: [AuthController],
   providers: [AuthService, PrismaService, MessageService, UtilService],
 })
