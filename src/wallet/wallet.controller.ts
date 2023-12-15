c492f361a4a1a4184e29e939e66b1e6137db37a7
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-15 10:41:26 +0100

    added controller to create wallet

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Req } from '@nestjs/common';
+import { Controller, Get, Query, Req, Post } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PaginationDto } from './dto/pagination.dto';
@@ -8,6 +8,12 @@ import { PaginationDto } from './dto/pagination.dto';
 export class WalletController {
   constructor(private walletService: WalletService) {}
 
+  @Post('')
+  @ApiOperation({ summary: 'Create wallet' })
+  createWallet(@Req() req) {
+    return this.walletService.createWallet(req.email);
+  }
+
   @Get('transactions')
   @ApiOperation({ summary: "Get all a wallet's transcations" })
   getWalletTransactions(@Query() dto: PaginationDto, @Req() req) {
@@ -15,7 +21,7 @@ export class WalletController {
   }
 
   @Get('details')
-  @ApiOperation({ summary: "Get all a wallet's transcations" })
+  @ApiOperation({ summary: 'Get wallet details' })
   getWalletDetails(@Req() req) {
     return this.walletService.getWalletDetails(req.user.email);
   }
