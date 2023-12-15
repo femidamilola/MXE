2c22327dec72e4a869d79fd536a996f1ecd7bf04
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-15 10:26:04 +0100

    setup wallet module

---
+++ src/wallet/wallet.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { WalletService } from './wallet.service';
+import { WalletController } from './wallet.controller';
+// import { APP_GUARD } from '@nestjs/core';
+// import { JwtGuard } from 'src/auth/guards/jwt.guard';
+
+@Module({
+  controllers: [WalletController],
+  providers: [
+    PrismaService,
+    WalletService,
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: JwtGuard,
+    // },
+  ],
+  imports: [],
+})
+export class WalletModule {}
