5a5ebee38576d81d9726d735ef50a938afac0964
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-20 00:54:57 +0100

    setup jwt guard

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
-// import { APP_GUARD } from '@nestjs/core';
-// import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Module({
   controllers: [WalletController],
   providers: [
     PrismaService,
     WalletService,
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: JwtGuard,
-    // },
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    },
   ],
   imports: [],
 })
