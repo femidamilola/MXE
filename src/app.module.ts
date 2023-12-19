34ef6a97ae2dedb65e68c04681144a516383aacb
femidamilola <fadebayo@kuilipaintegrated.com>
2023-12-20 00:37:57 +0100

    imported upload sevice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
+// import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { PrismaService } from './prisma.service';
 import { MessageService } from './utils/message.service';
@@ -7,10 +7,23 @@ import { ConfigModule } from '@nestjs/config';
 import { UtilService } from './utils/util.service';
 import { AuthModule } from './auth/auth.module';
 import { WalletModule } from './wallet/wallet.module';
+import { VerificationModule } from './verification/verification.module';
+import { UploadService } from './utils/upload.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, WalletModule],
-  controllers: [AuthController],
-  providers: [AuthService, PrismaService, MessageService, UtilService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    WalletModule,
+    VerificationModule,
+  ],
+  controllers: [],
+  providers: [
+    AuthService,
+    PrismaService,
+    MessageService,
+    UtilService,
+    UploadService,
+  ],
 })
 export class AppModule {}
