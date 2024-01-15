478b5ff534e0ed295b74d5eb7961baf859232734
femidamilola <fadebayo@kuilipaintegrated.com>
2024-01-15 11:32:15 +0100

    controller to get and create virtual cards

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Query, Req, Post, Param } from '@nestjs/common';
+import { Controller, Get, Query, Req, Post, Param, Body } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { PaginationDto } from './dto/pagination.dto';
+import { CreateVirtualCardDto } from './dto/wallet.dto';
 
 @ApiSecurity('JWT-auth')
 @ApiTags('Wallet')
@@ -51,5 +52,17 @@ export class WalletController {
   @ApiOperation({ summary: 'Get wallet details' })
   getWalletDetails(@Req() req) {
     return this.walletService.getWalletDetails(req.user.email);
+  }
+
+  @Get('virtual-card')
+  @ApiOperation({ summary: 'Get virtual card details' })
+  async getVirtualCard(@Req() req) {
+    return this.walletService.getVirtualCard(req.user.email);
+  }
+
+  @Post('virtual-card')
+  @ApiOperation({ summary: 'Create virtual card' })
+  async createVirtualCard(@Req() req, @Body() dto: CreateVirtualCardDto) {
+    return this.walletService.createVirtualCard(req.user.email, dto);
   }
 }
